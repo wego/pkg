0dc454fe4b4b7c83af44c9f23a11127876777841
Simans <simans@wego.com>
2021-10-19 15:03:11 +0800

    fix : fixing common package issues

--- collection/collection_test.go
+++ collection/collection_test.go
@@ -12,37 +12,37 @@ var currencies = []string{"USD", "MYR", "SGD", "INR"}
 func Test_Index(t *testing.T) {
 	assert := assert.New(t)
 
-	indexZero := common.Index(currencies, "USD")
+	indexZero := collection.Index(currencies, "USD")
 	assert.Equal(0, indexZero)
 
-	nonIndex := common.Index(currencies, "AED")
+	nonIndex := collection.Index(currencies, "AED")
 	assert.Equal(-1, nonIndex)
 }
 
 func Test_Include(t *testing.T) {
 	assert := assert.New(t)
-	assert.True(common.Include(currencies, "USD"))
-	assert.False(common.Include(currencies, "AED"))
+	assert.True(collection.Include(currencies, "USD"))
+	assert.False(collection.Include(currencies, "AED"))
 }
 
 func Test_Any(t *testing.T) {
 	assert := assert.New(t)
-	assert.True(common.Any(currencies, func(v string) bool {
+	assert.True(collection.Any(currencies, func(v string) bool {
 		return strings.HasPrefix(v, "U")
 	}))
-	assert.False(common.Any(currencies, func(v string) bool {
+	assert.False(collection.Any(currencies, func(v string) bool {
 		return strings.HasPrefix(v, "X")
 	}))
 }
 
 func Test_Filter(t *testing.T) {
 	assert := assert.New(t)
-	resultYes := common.Filter(currencies, func(v string) bool {
+	resultYes := collection.Filter(currencies, func(v string) bool {
 		return strings.Contains(v, "USD")
 	})
 	assert.Equal(1, len(resultYes))
 
-	resultNo := common.Filter(currencies, func(v string) bool {
+	resultNo := collection.Filter(currencies, func(v string) bool {
 		return strings.Contains(v, "JOD")
 	})
 	assert.Equal(0, len(resultNo))
