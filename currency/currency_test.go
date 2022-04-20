b111b47b56d8894762d246a891cf1b7c8350cb94
Viet Tran <tdviet12@gmail.com>
2022-04-20 11:50:17 +0700

    update currency factors map & rename function to make it clearer

--- currency/currency_test.go
+++ currency/currency_test.go
@@ -18,140 +18,140 @@ func Test_IsISO4217(t *testing.T) {
 	assert.False(currency.IsISO4217(""))
 }
 
-func Test_AmountToAmountInCents_Ok(t *testing.T) {
+func Test_ToMinorUnit_OK(t *testing.T) {
 	assert := assert.New(t)
 
-	rs, err := currency.AmountToAmountInCents("SGD", 25.12)
+	rs, err := currency.ToMinorUnit("SGD", 25.12)
 	assert.NoError(err)
 	assert.EqualValues(2512, rs)
 
-	rs, err = currency.AmountToAmountInCents("SGD", 25.123)
+	rs, err = currency.ToMinorUnit("SGD", 25.123)
 	assert.NoError(err)
 	assert.EqualValues(2512, rs)
 
-	rs, err = currency.AmountToAmountInCents("BHD", 25.123)
+	rs, err = currency.ToMinorUnit("BHD", 25.123)
 	assert.NoError(err)
 	assert.EqualValues(25123, rs)
 
-	rs, err = currency.AmountToAmountInCents("BHD", 25.1234)
+	rs, err = currency.ToMinorUnit("BHD", 25.1234)
 	assert.NoError(err)
 	assert.EqualValues(25123, rs)
 
-	rs, err = currency.AmountToAmountInCents("VND", 25.12345)
+	rs, err = currency.ToMinorUnit("VND", 25.12345)
 	assert.NoError(err)
 	assert.EqualValues(25, rs)
 
-	rs, err = currency.AmountToAmountInCents("Vnd", 25.12345)
+	rs, err = currency.ToMinorUnit("Vnd", 25.12345)
 	assert.NoError(err)
 	assert.EqualValues(25, rs)
 }
 
-func Test_AmountToAmountInCents_InvalidCurrency(t *testing.T) {
+func Test_ToMinorUnit_InvalidCurrency(t *testing.T) {
 	assert := assert.New(t)
 
-	rs, err := currency.AmountToAmountInCents("", 25.12)
+	rs, err := currency.ToMinorUnit("", 25.12)
 	assert.Error(err)
 	assert.Contains(err.Error(), "is not a valid ISO 4217 currency code")
 	assert.Zero(rs)
 
-	rs, err = currency.AmountToAmountInCents("sg", 25.12)
+	rs, err = currency.ToMinorUnit("sg", 25.12)
 	assert.Error(err)
 	assert.Contains(err.Error(), "is not a valid ISO 4217 currency code")
 	assert.Zero(rs)
 
-	rs, err = currency.AmountToAmountInCents(" ", 25.12)
+	rs, err = currency.ToMinorUnit(" ", 25.12)
 	assert.Error(err)
 	assert.Contains(err.Error(), "is not a valid ISO 4217 currency code")
 	assert.Zero(rs)
 
-	rs, err = currency.AmountToAmountInCents("      ", 25.12)
+	rs, err = currency.ToMinorUnit("      ", 25.12)
 	assert.Error(err)
 	assert.Contains(err.Error(), "is not a valid ISO 4217 currency code")
 	assert.Zero(rs)
 }
 
-func Test_AmountToAmountInCents_ZeroAmount(t *testing.T) {
+func Test_ToMinorUnit_ZeroAmount(t *testing.T) {
 	assert := assert.New(t)
 
-	rs, err := currency.AmountToAmountInCents("SGD", 0)
+	rs, err := currency.ToMinorUnit("SGD", 0)
 	assert.NoError(err)
 	assert.Zero(rs)
 }
 
-func Test_AmountToAmountInCents_InvalidAmount(t *testing.T) {
+func Test_ToMinorUnit_InvalidAmount(t *testing.T) {
 	assert := assert.New(t)
 
-	rs, err := currency.AmountToAmountInCents("USD", -0.0001)
+	rs, err := currency.ToMinorUnit("USD", -0.0001)
 	assert.Error(err)
 	assert.Contains(err.Error(), "invalid amount")
 	assert.Zero(rs)
 
-	rs, err = currency.AmountToAmountInCents("SGd", -0.0001)
+	rs, err = currency.ToMinorUnit("SGd", -0.0001)
 	assert.Error(err)
 	assert.Contains(err.Error(), "invalid amount")
 	assert.Zero(rs)
 }
 
-func Test_AmountInCentsToAmount_Ok(t *testing.T) {
+func Test_FromMinorUnit_OK(t *testing.T) {
 	assert := assert.New(t)
 
-	rs, err := currency.AmountInCentsToAmount("SGD", 2512)
+	rs, err := currency.FromMinorUnit("SGD", 2512)
 	assert.NoError(err)
 	assert.EqualValues(25.12, rs)
 
-	rs, err = currency.AmountInCentsToAmount("SGD", 25123)
+	rs, err = currency.FromMinorUnit("SGD", 25123)
 	assert.NoError(err)
 	assert.EqualValues(251.23, rs)
 
-	rs, err = currency.AmountInCentsToAmount("BHD", 25123)
+	rs, err = currency.FromMinorUnit("BHD", 25123)
 	assert.NoError(err)
 	assert.EqualValues(25.123, rs)
 
-	rs, err = currency.AmountInCentsToAmount("BHD", 251234)
+	rs, err = currency.FromMinorUnit("BHD", 251234)
 	assert.NoError(err)
 	assert.EqualValues(251.234, rs)
 
-	rs, err = currency.AmountInCentsToAmount("VND", 2512345)
+	rs, err = currency.FromMinorUnit("VND", 2512345)
 	assert.NoError(err)
 	assert.EqualValues(2512345, rs)
 
-	rs, err = currency.AmountInCentsToAmount("Vnd", 2512345)
+	rs, err = currency.FromMinorUnit("Vnd", 2512345)
 	assert.NoError(err)
 	assert.EqualValues(2512345, rs)
 }
 
-func Test_AmountInCentsToAmount_InvalidCurrency(t *testing.T) {
+func Test_FromMinorUnit_InvalidCurrency(t *testing.T) {
 	assert := assert.New(t)
 
-	rs, err := currency.AmountInCentsToAmount("", 2512)
+	rs, err := currency.FromMinorUnit("", 2512)
 	assert.Error(err)
 	assert.Contains(err.Error(), "is not a valid ISO 4217 currency code")
 	assert.Zero(rs)
 
-	rs, err = currency.AmountInCentsToAmount("sg", 2512)
+	rs, err = currency.FromMinorUnit("sg", 2512)
 	assert.Error(err)
 	assert.Contains(err.Error(), "is not a valid ISO 4217 currency code")
 	assert.Zero(rs)
 
-	rs, err = currency.AmountInCentsToAmount(" ", 2512)
+	rs, err = currency.FromMinorUnit(" ", 2512)
 	assert.Error(err)
 	assert.Contains(err.Error(), "is not a valid ISO 4217 currency code")
 	assert.Zero(rs)
 
-	rs, err = currency.AmountInCentsToAmount("      ", 2512)
+	rs, err = currency.FromMinorUnit("      ", 2512)
 	assert.Error(err)
 	assert.Contains(err.Error(), "is not a valid ISO 4217 currency code")
 	assert.Zero(rs)
 }
 
-func Test_AmountInCentsToAmount_ZeroAmount(t *testing.T) {
+func Test_FromMinorUnit_ZeroAmount(t *testing.T) {
 	assert := assert.New(t)
 
-	rs, err := currency.AmountInCentsToAmount("USD", 0)
+	rs, err := currency.FromMinorUnit("USD", 0)
 	assert.NoError(err)
 	assert.Zero(rs)
 
-	rs, err = currency.AmountInCentsToAmount("SGD", 0)
+	rs, err = currency.FromMinorUnit("SGD", 0)
 	assert.NoError(err)
 	assert.Zero(rs)
 }
