9861aefc987cf15e3beab66afb2f42afad1eab58
mysqto <my@mysq.to>
2022-06-10 14:21:13 +0800

    Add more indexes
    Signed-off-by: mysqto <my@mysq.to>

--- http/auth/migration_script.down.sql
+++ http/auth/migration_script.down.sql
@@ -4,3 +4,5 @@ DROP TABLE IF EXISTS auth_users;
 DROP TABLE IF EXISTS auth_roles;
 DROP INDEX IF EXISTS idx_auth_roles_name;
 DROP INDEX IF EXISTS idx_auth_users_email;
+DROP INDEX IF EXISTS idx_auth_user_roles_role_id_user_id;
+DROP INDEX IF EXISTS idx_auth_role_permissions_role_id_resource_method;
--- http/auth/migration_script.up.sql
+++ http/auth/migration_script.up.sql
@@ -38,5 +38,7 @@ CREATE TABLE IF NOT EXISTS auth_role_permissions
     deleted_at TIMESTAMP
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS idx_auth_roles_name ON auth_roles (name);
-CREATE UNIQUE INDEX IF NOT EXISTS idx_auth_users_email ON auth_users (email);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_auth_roles_name ON auth_roles (name) WHERE deleted_at IS NULL;;
+CREATE UNIQUE INDEX IF NOT EXISTS idx_auth_users_email ON auth_users (email) WHERE deleted_at IS NULL;;
+CREATE UNIQUE INDEX IF NOT EXISTS idx_auth_user_roles_role_id_user_id ON auth_user_roles (role_id, user_id) WHERE deleted_at IS NULL;
+CREATE UNIQUE INDEX IF NOT EXISTS idx_auth_role_permissions_role_id_resource_method ON auth_role_permissions (role_id, resource, method) WHERE deleted_at IS NULL;
