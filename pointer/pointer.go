e86315f673d14546e9a170727e18473db61f7b32
mysqto <my@mysq.to>
2022-09-27 09:30:54 +0800

    Add `ToNonZero`

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -1,6 +1,16 @@
 package pointer
 
+import "reflect"
+
 // To returns the pointer to the value
 func To[T any](value T) *T {
+	return &value
+}
+
+// ToNonZero returns the pointer to the value if the value is not zero
+func ToNonZero[T any](value T) *T {
+	if reflect.ValueOf(value).IsZero() {
+		return nil
+	}
 	return &value
 }
