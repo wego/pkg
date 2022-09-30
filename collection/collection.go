6348d91ada66f748db1e121cd9829222d6260605
Viet Tran <tdviet12@gmail.com>
2022-09-30 15:58:33 +0800

    fix typo

--- collection/collection.go
+++ collection/collection.go
@@ -1,7 +1,7 @@
 package collection
 
-// DeDup returns the input slice after removing duplicated items
-func DeDup[T comparable](in []T) (out []T) {
+// Dedup returns the input slice after removing duplicated items
+func Dedup[T comparable](in []T) (out []T) {
 	keys := make(map[T]bool)
 	for _, v := range in {
 		if found, _ := keys[v]; !found {
--- collection/collection_test.go
+++ collection/collection_test.go
@@ -7,19 +7,19 @@ import (
 	"github.com/wego/pkg/collection"
 )
 
-func Test_DeDup(t *testing.T) {
+func Test_Dedup(t *testing.T) {
 	as := assert.New(t)
 
-	stringOut := collection.DeDup([]string{"b", "a", "b", "c"})
+	stringOut := collection.Dedup([]string{"b", "a", "b", "c"})
 	as.ElementsMatch(stringOut, []string{"a", "b", "c"})
 
-	intOut := collection.DeDup([]int{3, 1, 2, 1, 2, 3, 3})
+	intOut := collection.Dedup([]int{3, 1, 2, 1, 2, 3, 3})
 	as.ElementsMatch(intOut, []int{1, 2, 3})
 
-	floatOut := collection.DeDup([]float64{3.1, 1.2, 2.3, 1.2, 3.1})
+	floatOut := collection.Dedup([]float64{3.1, 1.2, 2.3, 1.2, 3.1})
 	as.ElementsMatch(floatOut, []float64{1.2, 2.3, 3.1})
 
-	boolOut := collection.DeDup([]bool{true, false, false, true, true})
+	boolOut := collection.Dedup([]bool{true, false, false, true, true})
 	as.ElementsMatch(boolOut, []bool{true, false})
 
 	type comparableStruct struct {
@@ -37,6 +37,6 @@ func Test_DeDup(t *testing.T) {
 		{1, 1.0, "1", true},
 		{2, 2.0, "2", true},
 	}
-	structOut := collection.DeDup(structIn)
+	structOut := collection.Dedup(structIn)
 	as.ElementsMatch(structOut, expectedStructOut)
 }
