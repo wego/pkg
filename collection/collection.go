7e4153da50a26577777df78978e99bf9ffc26933
Chen Lei <my@mysq.to>
2022-10-06 18:05:06 +0800

    Update `collection` to use generic types (#59)

--- collection/collection.go
+++ collection/collection.go
@@ -3,6 +3,7 @@ package collection
 // Dedup returns the input slice after removing duplicated items
 func Dedup[T comparable](in []T) (out []T) {
 	keys := make(map[T]bool)
+	out = make([]T, 0)
 	for _, v := range in {
 		if found, _ := keys[v]; !found {
 			keys[v] = true
@@ -10,4 +11,89 @@ func Dedup[T comparable](in []T) (out []T) {
 		}
 	}
 	return
+}
+
+// IndexOf returns the first index of the target element t, or -1 if no match is found
+func IndexOf[T comparable](vs []T, t T) int {
+	for i, v := range vs {
+		if v == t {
+			return i
+		}
+	}
+	return -1
+}
+
+// Contains returns true if the target element t is in the slice.
+func Contains[T comparable](vs []T, t T) bool {
+	return IndexOf(vs, t) >= 0
+}
+
+// Any returns true if one of the element in the slice satisfies the predicate f
+func Any[T comparable](vs []T, f func(T) bool) bool {
+	for _, v := range vs {
+		if f(v) {
+			return true
+		}
+	}
+	return false
+}
+
+// All returns true if all the elements in the slice satisfy the predicate f
+func All[T comparable](vs []T, f func(T) bool) bool {
+	for _, v := range vs {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// Filter returns a new slice containing all in the slice that satisfy the predicate f
+func Filter[T comparable](vs []T, f func(T) bool) []T {
+	vsf := make([]T, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
+}
+
+// Map returns a new slice containing the results of applying the function f to each element in the original slice
+func Map[I, R comparable](vs []I, f func(I) R) []R {
+	vsm := make([]R, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+
+// MapI returns a new slice containing the results of applying the function f to each element in the original slice
+// FIXME: will deprecate this function in the future
+func MapI[T comparable](vs []T, f func(T) interface{}) []interface{} {
+	vsm := make([]interface{}, len(vs))
+	for i, v := range vs {
+		vsm[i] = f(v)
+	}
+	return vsm
+}
+
+// Equal checks whether 2 slices have the same items
+func Equal[T comparable](s1 []T, s2 []T) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	s1Map := make(map[T]bool)
+	for _, v := range s1 {
+		s1Map[v] = true
+	}
+
+	for _, v := range s2 {
+		if !s1Map[v] {
+			return false
+		}
+	}
+
+	return true
 }
--- collection/string_test.go
+++ collection/string_test.go
@@ -3,7 +3,6 @@ package collection_test
 import (
 	"github.com/stretchr/testify/assert"
 	"github.com/wego/pkg/collection"
-	"strings"
 	"testing"
 )
 
@@ -14,89 +13,21 @@ var (
 )
 
 func Test_Index(t *testing.T) {
-	assert := assert.New(t)
+	assertions := assert.New(t)
 
 	indexZero := collection.Index(currencies, "USD")
-	assert.Equal(0, indexZero)
+	assertions.Equal(0, indexZero)
+	assertions.Equal(indexZero, collection.IndexOf(currencies, "USD"))
 
 	nonIndex := collection.Index(currencies, "AED")
-	assert.Equal(-1, nonIndex)
+	assertions.Equal(-1, nonIndex)
+	assertions.Equal(nonIndex, collection.IndexOf(currencies, "AED"))
 }
 
 func Test_Include(t *testing.T) {
-	assert := assert.New(t)
-	assert.True(collection.Include(currencies, "USD"))
-	assert.False(collection.Include(currencies, "AED"))
-}
-
-func Test_Any(t *testing.T) {
-	assert := assert.New(t)
-	assert.True(collection.Any(currencies, func(v string) bool {
-		return strings.HasPrefix(v, "U")
-	}))
-	assert.False(collection.Any(currencies, func(v string) bool {
-		return strings.HasPrefix(v, "X")
-	}))
-}
-
-func Test_Filter(t *testing.T) {
-	assert := assert.New(t)
-	resultYes := collection.Filter(currencies, func(v string) bool {
-		return strings.Contains(v, "USD")
-	})
-	assert.Equal(2, len(resultYes))
-
-	resultNo := collection.Filter(currencies, func(v string) bool {
-		return strings.Contains(v, "JOD")
-	})
-	assert.Equal(0, len(resultNo))
-}
-
-func Test_All(t *testing.T) {
-	assert := assert.New(t)
-	resultNo := collection.All(strs, func(v string) bool {
-		return strings.HasPrefix(v, "p")
-	})
-	resultYes := collection.All(allStrs, func(v string) bool {
-		return strings.HasPrefix(v, "p")
-	})
-	assert.False(resultNo)
-	assert.True(resultYes)
-}
-
-func Test_Map(t *testing.T) {
-	assert := assert.New(t)
-	resultYes := collection.Map(strs, strings.ToUpper)
-	assert.Equal(resultYes[0], "PEACH")
-}
-
-func Test_MapI(t *testing.T) {
-	assert := assert.New(t)
-	resultYes := collection.MapI(strs, func(s string) interface{} {
-		return strings.ToUpper(s)
-	})
-	assert.Equal(resultYes[0], "PEACH")
-	assert.Equal(resultYes[1], "APPLE")
-}
-
-func Test_Distinct(t *testing.T) {
-	assert := assert.New(t)
-	distinct := collection.Distinct(currencies)
-	assert.Equal(distinct, []string{"USD", "MYR", "SGD", "INR"})
-	distinct = collection.Distinct(distinct)
-	assert.Equal(distinct, []string{"USD", "MYR", "SGD", "INR"})
-}
-
-func Test_Equal(t *testing.T) {
-	assert := assert.New(t)
-	s1 := []string{"1", "2"}
-	s2 := []string{"1", "2", "3"}
-	s3 := []string{"1"}
-	s4 := []string{"1", "3"}
-	s5 := []string{"2", "1"}
-	assert.True(collection.Equal(s1, s1))
-	assert.False(collection.Equal(s1, s2))
-	assert.False(collection.Equal(s1, s3))
-	assert.False(collection.Equal(s1, s4))
-	assert.True(collection.Equal(s1, s5))
+	assertions := assert.New(t)
+	assertions.True(collection.Include(currencies, "USD"))
+	assertions.True(collection.Contains(currencies, "USD"))
+	assertions.False(collection.Include(currencies, "AED"))
+	assertions.False(collection.Contains(currencies, "AED"))
 }
