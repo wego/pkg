54ccba4c9ee64a33644dcbff0c0dae167cbe3bff
mysqto <my@mysq.to>
2022-11-03 15:35:24 +0800

    Add PEM `Read` `Write` for keys

---
+++ encryption/ecies/keys_test.go
@@ -0,0 +1,52 @@
+package ecies_test
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/wego/pkg/encryption/ecies"
+	"testing"
+)
+
+const (
+	privateKey = `
+-----BEGIN EC PRIVATE KEY-----
+MIHcAgEBBEIBJ05YGc5gmMUq1l2atl3HqQkyrGQbOH9v8nK/hhwM4VpbmRPUI/xF
+zx3F5zQW0DZa+TTvXwzd5bJtUjQ75nzYcUmgBwYFK4EEACOhgYkDgYYABAE6zyKf
+6PUGDIOHnW+TwzYxGBUq1TCjkxHr8Mda+5FLxONdCD/Gc4S9wbxwQecKSChaIgPQ
+E1QHr/VAE5vBfX7nagDxGqL/0HMPUIxHbG4fYyVw6O5mCuA8JiMfcZOXvsZhqTNb
+vMLQIXPinzVBR4u4lFQWRLttAvYC9JGO7Ar4smKXQQ==
+-----END EC PRIVATE KEY-----
+`
+	publicKey = `
+-----BEGIN PUBLIC KEY-----
+MIGbMBAGByqGSM49AgEGBSuBBAAjA4GGAAQBOs8in+j1BgyDh51vk8M2MRgVKtUw
+o5MR6/DHWvuRS8TjXQg/xnOEvcG8cEHnCkgoWiID0BNUB6/1QBObwX1+52oA8Rqi
+/9BzD1CMR2xuH2MlcOjuZgrgPCYjH3GTl77GYakzW7zC0CFz4p81QUeLuJRUFkS7
+bQL2AvSRjuwK+LJil0E=
+-----END PUBLIC KEY-----
+`
+)
+
+func Test_Load_Unload_Keys_Ok(t *testing.T) {
+	assert := assert.New(t)
+	priv, err := ecies.PrivateKeyFromPEMString(privateKey)
+	assert.NoError(err)
+	assert.NotNil(priv)
+
+	pub, err := ecies.PublicKeyFromPEMString(publicKey)
+	assert.NoError(err)
+	assert.NotNil(pub)
+	assert.Equal(priv.Pub.Hex(), pub.Hex())
+
+	toEncrypt := []byte("hello world")
+	encrypted, err := ecies.Encrypt(toEncrypt, pub, nil, nil)
+	assert.NoError(err)
+	assert.NotNil(encrypted)
+	fmt.Println(hex.EncodeToString(encrypted))
+
+	decrypted, err := ecies.Decrypt(encrypted, priv, nil, nil)
+	assert.NoError(err)
+	assert.NotNil(decrypted)
+	assert.Equal(toEncrypt, decrypted)
+}
--- encryption/ecies/privatekey.go
+++ encryption/ecies/privatekey.go
@@ -1,12 +1,16 @@
 package ecies
 
 import (
+	"crypto/ecdsa"
 	"crypto/elliptic"
+	"crypto/x509"
 	"encoding/base64"
 	"encoding/hex"
-	"math/big"
-
+	"encoding/pem"
+	"fmt"
 	"github.com/wego/pkg/errors"
+	"math/big"
+	"os"
 )
 
 // PrivateKey ...
@@ -14,6 +18,10 @@ type PrivateKey struct {
 	Pub *PublicKey
 	k   *big.Int
 }
+
+const (
+	ecPrivateKeyBlockType = "EC PRIVATE KEY"
+)
 
 // PrivateKeyFromBytes parses a private key from its raw bytes
 func PrivateKeyFromBytes(b []byte, curve elliptic.Curve) *PrivateKey {
@@ -51,6 +59,45 @@ func PrivateKeyFromHex(hexKey string, curve elliptic.Curve) (*PrivateKey, error)
 	return PrivateKeyFromBytes(b, curve), nil
 }
 
+// PrivateKeyFromPEMBytes parses a private key from a PEM bytes
+func PrivateKeyFromPEMBytes(bytes []byte) (*PrivateKey, error) {
+	block, _ := pem.Decode(bytes)
+	if block == nil {
+		return nil, errors.New("failed to parse PEM block containing the key")
+	}
+
+	priv, err := x509.ParseECPrivateKey(block.Bytes)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("failed to parse DER encoded public key: %s", hex.EncodeToString(bytes)), err)
+	}
+
+	return &PrivateKey{
+		Pub: &PublicKey{
+			curve: priv.Curve,
+			Point: &Point{
+				X: priv.X,
+				Y: priv.Y,
+			},
+		},
+		k: priv.D,
+	}, nil
+}
+
+// PrivateKeyFromPEMString parses a private key from a PEM string
+func PrivateKeyFromPEMString(str string) (*PrivateKey, error) {
+	return PrivateKeyFromPEMBytes([]byte(str))
+}
+
+// PrivateKeyFromPEMFile parses a private key from a PEM file
+func PrivateKeyFromPEMFile(file string) (*PrivateKey, error) {
+	bytes, err := os.ReadFile(file)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("failed to read pem file: %s", file), err)
+	}
+
+	return PrivateKeyFromPEMBytes(bytes)
+}
+
 // Bytes returns private key raw bytes
 func (priv *PrivateKey) Bytes() []byte {
 	return priv.k.Bytes()
@@ -64,4 +111,27 @@ func (priv *PrivateKey) Base64() string {
 // Hex returns private key bytes in hex form
 func (priv *PrivateKey) Hex() string {
 	return hex.EncodeToString(priv.Bytes())
+}
+
+// PEM returns private key in PEM form - string
+func (priv *PrivateKey) PEM() string {
+	return string(priv.ToPEM())
+}
+
+// ToPEM returns private key in PEM form - bytes
+func (priv *PrivateKey) ToPEM() []byte {
+	x509Encoded, _ := x509.MarshalECPrivateKey(&ecdsa.PrivateKey{
+		PublicKey: ecdsa.PublicKey{
+			Curve: priv.Pub.curve,
+			X:     priv.Pub.X,
+			Y:     priv.Pub.Y,
+		},
+		D: priv.k,
+	})
+	return pem.EncodeToMemory(&pem.Block{Type: ecPrivateKeyBlockType, Bytes: x509Encoded})
+}
+
+// SavePEMFile saves private key to a PEM file
+func (priv *PrivateKey) SavePEMFile(file string) error {
+	return os.WriteFile(file, priv.ToPEM(), 0644)
 }
