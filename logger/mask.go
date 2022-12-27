433493df43df47bb0e9bcb6938a52be982cec0b0
Aaron <aaron.asuncion@wego.com>
2022-12-27 18:02:31 +0800

    PAY-770-support-masking-request-log (#69)
    * PAY-770-support-masking-request-log
    
    - added masking functions for xml or json string fields
    - updated go version to 1.18
    - added tests
    
    https://wegomushi.atlassian.net/browse/PAY-770
    
    * PAY-770-support-masking-request-log
    
    - updated MaskJSON comment
    
    * PAY-770-support-masking-request-log
    
    - fixed typo
    
    * PAY-770-support-masking-request-log
    
    - added MaskRestrictionType for special dynamic fields that should be masked depending on the value.
    - added tests.
    
    * PAY-770-support-masking-request-log
    
    - added new line to test files

---
+++ logger/mask.go
@@ -0,0 +1,221 @@
+package logger
+
+import (
+	"net/mail"
+	"strings"
+
+	"github.com/antchfx/xmlquery"
+	"github.com/valyala/fastjson"
+	"github.com/wego/pkg/collection"
+	"github.com/wego/pkg/errors"
+)
+
+// MaskRestrictionType the type of text to mask, will only mask if text is of the specified type
+type MaskRestrictionType string
+
+const (
+	// MaskRestrictionTypeEmail will only mask email text
+	MaskRestrictionTypeEmail MaskRestrictionType = "email"
+)
+
+// MaskData the data as well as the information on how to mask the data
+type MaskData struct {
+	FirstCharsToShow int
+	LastCharsToShow  int
+	RestrictionType  MaskRestrictionType
+	CharsToIgnore    []rune
+	XMLTag           string
+	JSONKeys         []string
+}
+
+// MaskXML masks parts of the inner text of tags from the input XML with replacement
+func MaskXML(xml, maskChar string, toMasks []MaskData) string {
+	maskChar = maskCharOrDefault(maskChar)
+
+	doc, err := xmlquery.Parse(strings.NewReader(xml))
+	if err != nil {
+		return errors.New("invalid XML input", err).Error()
+	}
+	out := findTagAndMaskMulti(doc, maskChar, toMasks)
+
+	return out
+}
+
+func findTagAndMaskMulti(doc *xmlquery.Node, maskChar string, toMasks []MaskData) string {
+	for _, toMask := range toMasks {
+		findTagAndMask(doc, maskChar, toMask)
+	}
+	return doc.OutputXML(true)
+}
+
+func findTagAndMask(doc *xmlquery.Node, maskChar string, toMask MaskData) {
+	nodes := xmlquery.Find(doc, "//"+toMask.XMLTag)
+	for _, node := range nodes {
+		tagValue := node.InnerText()
+		if strings.TrimSpace(tagValue) != "" {
+			var nodeToUpdate *xmlquery.Node
+			// get specific node that contains the value to update
+			for child := node.FirstChild; child != nil; child = child.NextSibling {
+				nodeToUpdate = child
+			}
+			maskedVal := getMaskedValue(maskChar, tagValue, toMask)
+			nodeToUpdate.Data = maskedVal
+		}
+	}
+}
+
+// MaskJSON mask parts of the json key paths value from the input json with replacement
+//
+// Example:
+//
+// 		input = `
+// 		{
+// 			"first": "first value",
+// 			"second": {
+// 				"first": "1st of second",
+// 				"second": "second@wego.com",
+// 				"third": {
+// 					"first": "1st of second third",
+// 					"second": "2nd of second third",
+// 					"third": "3rd of second third",
+// 				}
+// 			}
+// 		}`
+// 		maskData := []logger.MaskData{
+//			{
+//				JSONKey:         []string{"first"},
+//				FistCharsToShow: 3,
+//				LastCharsToShow: 6,
+//			},
+//			{
+//				JSONKey:         []string{"second", "second"},
+//				FistCharsToShow: 2,
+//				LastCharsToShow: 3,
+//				CharsToIgnore:   []rune{'@'},
+//			},
+//			{
+//				JSONKey:         []string{"second", "third", "first"},
+//				FistCharsToShow: 3,
+//				LastCharsToShow: 1,
+//			},
+//		}
+//		MaskJSON(input, "!", maskData) will return
+//		{
+//			"first": "fir!! value",
+//			"second": {
+//				"first": "1st of second",
+//				"second": "se!!!!@!!!!!com",
+//				"third": {
+//					"first": "1st!!!!!!!!!!!!!!!d",
+//					"second": "2nd of second third",
+//					"third": "3rd of second third",
+//				}
+//			}
+//		}
+func MaskJSON(json, maskChar string, toMasks []MaskData) string {
+	maskChar = maskCharOrDefault(maskChar)
+
+	var p fastjson.Parser
+	root, err := p.Parse(json)
+	if err != nil {
+		return err.Error()
+	}
+
+	for _, toMask := range toMasks {
+		l := len(toMask.JSONKeys)
+		switch {
+		case l == 1:
+			if exist := root.Exists(toMask.JSONKeys[0]); exist {
+				// currently do not support masking for non-string values
+				value := getJSONValue(root.Get(toMask.JSONKeys[0]))
+				if value != "" {
+					maskedVal := getMaskedValue(maskChar, value, toMask)
+					replacement := fastjson.MustParse(`"` + maskedVal + `"`)
+					root.Set(toMask.JSONKeys[0], replacement)
+				}
+			}
+		case l > 1:
+			if exist := root.Exists(toMask.JSONKeys...); exist {
+				// get the parent obj then replace the value
+				v := root.Get(toMask.JSONKeys[:l-1]...)
+
+				// currently do not support masking for non-string values
+				value := getJSONValue(v.Get(toMask.JSONKeys[l-1]))
+				if value != "" {
+					maskedVal := getMaskedValue(maskChar, value, toMask)
+					replacement := fastjson.MustParse(`"` + maskedVal + `"`)
+					v.Set(toMask.JSONKeys[l-1], replacement)
+				}
+			}
+		}
+	}
+
+	out := root.MarshalTo([]byte{})
+	return string(out)
+}
+
+func getJSONValue(jsonVal *fastjson.Value) string {
+	val := ""
+	if jsonVal != nil {
+		bytes := jsonVal.GetStringBytes()
+		if len(bytes) > 0 {
+			val = string(bytes[:])
+		}
+	}
+
+	return val
+}
+
+func maskCharOrDefault(maskChar string) string {
+	if maskChar == "" {
+		maskChar = defaultMaskChar
+	}
+	return maskChar
+}
+
+func getMaskedValue(maskChar, valueToReplace string, toMask MaskData) string {
+	negativeCharsToShow := toMask.FirstCharsToShow < 0 || toMask.LastCharsToShow < 0
+	if negativeCharsToShow || !willMask(valueToReplace, toMask.RestrictionType) {
+		return valueToReplace
+	}
+
+	totalCharsToShow := toMask.FirstCharsToShow + toMask.LastCharsToShow
+	valueToReplaceLen := len(valueToReplace)
+	lastIndexToShowStart := valueToReplaceLen - toMask.LastCharsToShow
+
+	// check if need to mask
+	repeatTimes := valueToReplaceLen - totalCharsToShow
+	if repeatTimes <= 0 {
+		return valueToReplace
+	}
+
+	valToMask := valueToReplace[toMask.FirstCharsToShow:lastIndexToShowStart]
+	var sb strings.Builder
+	for _, c := range valToMask {
+		// do not mask characters that should be ignored like '@'
+		if collection.Contains(toMask.CharsToIgnore, c) {
+			sb.WriteRune(c)
+		} else {
+			sb.WriteString(maskChar)
+		}
+	}
+
+	replacement := sb.String()
+	maskedVal := valueToReplace[:toMask.FirstCharsToShow] + replacement + valueToReplace[lastIndexToShowStart:]
+
+	return maskedVal
+}
+
+func willMask(valueToReplace string, restrictionType MaskRestrictionType) bool {
+	if restrictionType == "" || valueToReplace == "" {
+		return true
+	}
+
+	switch restrictionType {
+	case MaskRestrictionTypeEmail:
+		_, err := mail.ParseAddress(valueToReplace)
+		return err == nil
+	}
+
+	return true
+}
---
+++ logger/mask_test.go
@@ -0,0 +1,286 @@
+package logger_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/antchfx/xmlquery"
+	"github.com/stretchr/testify/assert"
+	"github.com/wego/pkg/errors"
+	"github.com/wego/pkg/logger"
+)
+
+const (
+	testFileDir = "./testfiles/mask/"
+)
+
+func Test_MaskXML_ReturnErrorText_WithInvalidInput(t *testing.T) {
+	assert := assert.New(t)
+
+	output := logger.MaskXML("<wego", "", []logger.MaskData{})
+	assert.Contains(output, "invalid XML input")
+}
+
+func Test_MaskXML_DoNothing_WhenInputEmptyTags(t *testing.T) {
+	assert := assert.New(t)
+
+	input, err := parseXMLToString("mask_input.xml")
+
+	output := logger.MaskXML(input, "", []logger.MaskData{})
+	assert.NoError(err)
+	assert.Equal(input, output)
+}
+
+func Test_MaskXML_Ok(t *testing.T) {
+	assert := assert.New(t)
+	maskData := []logger.MaskData{
+		{
+			XMLTag:           "Test1",
+			FirstCharsToShow: 4,
+			LastCharsToShow:  6,
+		},
+		{
+			XMLTag:           "Number",
+			FirstCharsToShow: 4,
+			LastCharsToShow:  6,
+		},
+		{
+			XMLTag:           "Email",
+			FirstCharsToShow: 3,
+			LastCharsToShow:  5,
+			CharsToIgnore:    []rune{'@'},
+		},
+		{
+			XMLTag:           "SomethingThatDoesNotExists",
+			FirstCharsToShow: 2,
+			LastCharsToShow:  6,
+		},
+		{
+			XMLTag:           "AgentID",
+			FirstCharsToShow: 0,
+			LastCharsToShow:  0,
+		},
+		{
+			XMLTag:           "ClientID",
+			FirstCharsToShow: 2,
+			LastCharsToShow:  0,
+		},
+		{
+			XMLTag:           "BookerID",
+			FirstCharsToShow: 0,
+			LastCharsToShow:  2,
+		},
+		{
+			XMLTag:           "Phone",
+			FirstCharsToShow: 4,
+			LastCharsToShow:  3,
+			CharsToIgnore:    []rune{'$'},
+		},
+		{
+			XMLTag:           "UserID1",
+			FirstCharsToShow: 5,
+			LastCharsToShow:  6,
+			CharsToIgnore:    []rune{'@'},
+			RestrictionType:  logger.MaskRestrictionTypeEmail,
+		},
+		{
+			XMLTag:           "UserID2",
+			FirstCharsToShow: 5,
+			LastCharsToShow:  6,
+			CharsToIgnore:    []rune{'@'},
+			RestrictionType:  logger.MaskRestrictionTypeEmail,
+		},
+	}
+	input, err := parseXMLToString("mask_input.xml")
+	assert.NoError(err)
+
+	for _, testCase := range []struct {
+		expectedTestFile string
+		replacement      string
+	}{
+		{
+			expectedTestFile: "expected_mask_input.xml",
+			replacement:      "|",
+		},
+		{
+			expectedTestFile: "expected_default_mask_input.xml",
+		},
+	} {
+		output := logger.MaskXML(input, testCase.replacement, maskData)
+		expected, err := parseXMLToString(testCase.expectedTestFile)
+		assert.NoError(err)
+		assert.Equal(expected, output)
+	}
+}
+
+func parseXMLToString(xmlFileName string) (string, error) {
+	file, err := os.ReadFile(testFileDir + xmlFileName)
+	if err != nil {
+		return "", err
+	}
+
+	doc, err := xmlquery.Parse(bytes.NewReader(file))
+	if err != nil {
+		return "", errors.New("invalid XML input", err)
+	}
+
+	return doc.OutputXML(true), nil
+}
+
+func Test_MaskJSON_InvalidInput(t *testing.T) {
+	assert := assert.New(t)
+
+	output := logger.MaskJSON("<123 invalid }", "", []logger.MaskData{})
+	assert.Contains(output, "cannot parse JSON")
+}
+
+func Test_MaskJSON_DoNothing_WhenKeysNotFound(t *testing.T) {
+	assert := assert.New(t)
+	var compactOutput, compactExpectedOutput bytes.Buffer
+	input, err := parseJSONToString("mask_input.json")
+	assert.NoError(err)
+
+	for _, testCase := range []struct {
+		maskData []logger.MaskData
+	}{
+		{
+			maskData: nil,
+		},
+		{
+			maskData: []logger.MaskData{},
+		},
+		{
+			maskData: []logger.MaskData{
+				{
+					JSONKeys:         []string{"yo"},
+					FirstCharsToShow: 4,
+					LastCharsToShow:  6,
+				},
+				{
+					JSONKeys:         []string{"source", "billing_address", "whatsup"},
+					FirstCharsToShow: 3,
+					LastCharsToShow:  5,
+					CharsToIgnore:    []rune{'@'},
+				},
+				{
+					JSONKeys:         []string{"3ds", "hi"},
+					FirstCharsToShow: 2,
+					LastCharsToShow:  6,
+				},
+			},
+		},
+	} {
+
+		// not provide MaskData
+		output := logger.MaskJSON(input, "", testCase.maskData)
+		err = json.Compact(&compactOutput, []byte(output))
+		assert.NoError(err)
+		err = json.Compact(&compactExpectedOutput, []byte(input))
+		assert.NoError(err)
+		assert.Equal(compactExpectedOutput, compactOutput)
+	}
+}
+
+func Test_MaskJSON_Ok(t *testing.T) {
+	maskData := []logger.MaskData{
+		{
+			JSONKeys:         []string{"source", "phone", "number"},
+			FirstCharsToShow: 2,
+			LastCharsToShow:  4,
+		},
+		{
+			JSONKeys:         []string{"yo"},
+			FirstCharsToShow: 4,
+			LastCharsToShow:  6,
+		},
+		{
+			JSONKeys:         []string{"destination", "phone", "number"},
+			FirstCharsToShow: 2,
+			LastCharsToShow:  4,
+			CharsToIgnore:    []rune{'+'},
+		},
+		{
+			JSONKeys:         []string{"test1"},
+			FirstCharsToShow: 0,
+			LastCharsToShow:  0,
+		},
+		{
+			JSONKeys:         []string{"test2"},
+			FirstCharsToShow: 2,
+			LastCharsToShow:  0,
+		},
+		{
+			JSONKeys:         []string{"test3"},
+			FirstCharsToShow: 0,
+			LastCharsToShow:  3,
+		},
+		{
+			JSONKeys:         []string{"test4"},
+			FirstCharsToShow: 3,
+			LastCharsToShow:  3,
+		},
+		{
+			JSONKeys:         []string{"customer", "email"},
+			FirstCharsToShow: 5,
+			LastCharsToShow:  3,
+			CharsToIgnore:    []rune{'@'},
+		},
+		{
+			JSONKeys:         []string{"shipping", "phone", "number"},
+			FirstCharsToShow: 2,
+			LastCharsToShow:  1,
+		},
+		{
+			JSONKeys:         []string{"risk", "userId1"},
+			FirstCharsToShow: 2,
+			LastCharsToShow:  7,
+			CharsToIgnore:    []rune{'@'},
+		},
+		{
+			JSONKeys:         []string{"risk", "userId2"},
+			FirstCharsToShow: 2,
+			LastCharsToShow:  7,
+			RestrictionType:  logger.MaskRestrictionTypeEmail,
+			CharsToIgnore:    []rune{'@'},
+		},
+	}
+
+	assert := assert.New(t)
+	var compactOutput, compactExpectedOutput bytes.Buffer
+
+	input, err := parseJSONToString("mask_input.json")
+	assert.NoError(err)
+
+	for _, testCase := range []struct {
+		expectedTestFile string
+		replacement      string
+	}{
+		{
+			expectedTestFile: "expected_mask_input.json",
+			replacement:      "|",
+		},
+		{
+			expectedTestFile: "expected_default_mask_input.json",
+		},
+	} {
+		output := logger.MaskJSON(input, testCase.replacement, maskData)
+		err := json.Compact(&compactOutput, []byte(output))
+		assert.NoError(err)
+
+		expected, err := parseJSONToString(testCase.expectedTestFile)
+		assert.NoError(err)
+		err = json.Compact(&compactExpectedOutput, []byte(expected))
+		assert.NoError(err)
+		assert.Equal(compactExpectedOutput, compactOutput)
+	}
+}
+
+func parseJSONToString(jsonFileName string) (string, error) {
+	file, err := os.ReadFile(testFileDir + jsonFileName)
+	if err != nil {
+		return "", err
+	}
+	return string(file[:]), nil
+}
