f01e231c63557571c4390af32df11ff718f07097
Aaron Asuncion <aaron.asuncion@wego.com>
2023-04-11 14:13:32 +0800

    PAY-891-razorpay-get-payment-integration
    - code review

--- datetime/epochtime.go
+++ datetime/epochtime.go
@@ -12,8 +12,8 @@ type EpochTime time.Time
 // UnmarshalJSON Parses the json string epoch time to time.Time
 func (e *EpochTime) UnmarshalJSON(b []byte) (err error) {
 	s := strings.Trim(string(b), "\"")
-	epochStr, err := strconv.ParseInt(s, 10, 64)
-	convertedTime := time.Unix(0, epochStr*int64(time.Millisecond))
+	epoch, err := strconv.ParseInt(s, 10, 64)
+	convertedTime := time.UnixMilli(epoch)
 
 	*e = EpochTime(convertedTime)
 
