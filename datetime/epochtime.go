7bb237cea1588cfd5ce708f50b987e92ca16f44c
mysqto <my@mysq.to>
2023-04-20 13:51:33 +0800

    Add `EpochTimeSeconds`

--- datetime/epochtime.go
+++ datetime/epochtime.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-// EpochTime represents time from string epoch format
+// EpochTime represents time from string epoch format in milliseconds
 type EpochTime time.Time
 
 // UnmarshalJSON Parses the json string epoch time to time.Time
@@ -16,6 +16,20 @@ func (e *EpochTime) UnmarshalJSON(b []byte) (err error) {
 	convertedTime := time.UnixMilli(epoch)
 
 	*e = EpochTime(convertedTime)
+
+	return
+}
+
+// EpochTimeSeconds represents time from string epoch format in seconds
+type EpochTimeSeconds time.Time
+
+// UnmarshalJSON Parses the json string epoch time to time.Time
+func (e *EpochTimeSeconds) UnmarshalJSON(b []byte) (err error) {
+	s := strings.Trim(string(b), "\"")
+	epoch, err := strconv.ParseInt(s, 10, 64)
+	convertedTime := time.Unix(epoch, 0)
+
+	*e = EpochTimeSeconds(convertedTime)
 
 	return
 }
