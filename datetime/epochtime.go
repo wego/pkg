a45ac69a02d3d03eb8949d822bba9ca9bbcb1e08
Chen Lei <my@mysq.to>
2023-04-26 14:48:58 +0800

    Fix `datetime` `null` error (#111)
    * Fix `datetime` `null` error
    
    * fix comments
    
    * fix comments

--- datetime/epochtime.go
+++ datetime/epochtime.go
@@ -6,17 +6,22 @@ import (
 	"time"
 )
 
+const (
+	nullString = "null"
+)
+
 // EpochTime represents time from string epoch format in milliseconds
 type EpochTime time.Time
 
 // UnmarshalJSON Parses the json string epoch time to time.Time
 func (e *EpochTime) UnmarshalJSON(b []byte) (err error) {
-	s := strings.Trim(string(b), "\"")
-	epoch, err := strconv.ParseInt(s, 10, 64)
-	convertedTime := time.UnixMilli(epoch)
+	var epoch int64
 
-	*e = EpochTime(convertedTime)
-
+	epoch, err = parseEpochTime(b)
+	if err != nil || epoch == 0 {
+		return
+	}
+	*e = EpochTime(time.UnixMilli(epoch))
 	return
 }
 
@@ -25,11 +30,21 @@ type EpochTimeSeconds time.Time
 
 // UnmarshalJSON Parses the json string epoch time to time.Time
 func (e *EpochTimeSeconds) UnmarshalJSON(b []byte) (err error) {
+	var epoch int64
+
+	epoch, err = parseEpochTime(b)
+	if err != nil || epoch == 0 {
+		return
+	}
+	*e = EpochTimeSeconds(time.Unix(epoch, 0))
+	return
+}
+
+func parseEpochTime(b []byte) (epoch int64, err error) {
 	s := strings.Trim(string(b), "\"")
-	epoch, err := strconv.ParseInt(s, 10, 64)
-	convertedTime := time.Unix(epoch, 0)
-
-	*e = EpochTimeSeconds(convertedTime)
-
+	if strings.ToLower(s) != nullString {
+		epoch, err = strconv.ParseInt(s, 10, 64)
+		return
+	}
 	return
 }
