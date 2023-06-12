13bb24b593b6f79b29b60bba495259a4c79bb68e
mysqto <my@mysq.to>
2023-06-12 10:36:44 +0800

    add more test case

--- http/wegin/engine_test.go
+++ http/wegin/engine_test.go
@@ -12,13 +12,14 @@ import (
 )
 
 type testStruct struct {
-	Reference string `json:"reference" binding:"required,alphanum_with_underscore_or_dash"`
+	Reference        string  `json:"reference" binding:"required,alphanum_with_underscore_or_dash"`
+	ReferencePointer *string `json:"reference_pointer" binding:"required,alphanum_with_underscore_or_dash"`
 }
 
 func testHandler(c *gin.Context) {
 	var b testStruct
 	if err := c.ShouldBindJSON(&b); err == nil {
-		c.JSON(http.StatusOK, gin.H{"reference": b.Reference})
+		c.JSON(http.StatusOK, b)
 	} else {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	}
@@ -46,69 +47,99 @@ func (s *EngineSuite) SetupTest() {
 
 func (s *EngineSuite) Test_AlphaNumWithUnderscoreOrDash() {
 	for _, testCase := range []struct {
-		name           string
-		referenceKey   string
-		referenceValue string
-		expectedStatus int
-		expectedBodies []string
+		name            string
+		refKey          string
+		refValue        string
+		refPointerKey   string
+		refPointerValue string
+		expectedStatus  int
+		expectedBodies  []string
 	}{
 		{
 			name:           "empty",
-			referenceKey:   "reference",
-			referenceValue: "",
+			refKey:         "reference",
+			refValue:       "",
 			expectedStatus: http.StatusBadRequest,
 			expectedBodies: []string{
 				"Key: 'testStruct.Reference' Error:Field validation for 'Reference' failed on the 'required' tag",
+				"Key: 'testStruct.ReferencePointer' Error:Field validation for 'ReferencePointer' failed on the 'required' tag",
 			},
 		},
 		{
-			name:           "all space",
-			referenceKey:   "reference",
-			referenceValue: "   ",
-			expectedStatus: http.StatusBadRequest,
+			name:            "empty value",
+			refPointerKey:   "reference_pointer",
+			refPointerValue: "",
+			expectedStatus:  http.StatusBadRequest,
+			expectedBodies: []string{
+				"Key: 'testStruct.Reference' Error:Field validation for 'Reference' failed on the 'required' tag",
+				"Key: 'testStruct.ReferencePointer' Error:Field validation for 'ReferencePointer' failed on the 'alphanum_with_underscore_or_dash' tag",
+			},
+		},
+		{
+			name:            "all space",
+			refKey:          "reference",
+			refValue:        "   ",
+			refPointerKey:   "reference_pointer",
+			refPointerValue: "   ",
+			expectedStatus:  http.StatusBadRequest,
 			expectedBodies: []string{
 				"Key: 'testStruct.Reference' Error:Field validation for 'Reference' failed on the 'alphanum_with_underscore_or_dash' tag",
+				"Key: 'testStruct.ReferencePointer' Error:Field validation for 'ReferencePointer' failed on the 'alphanum_with_underscore_or_dash' tag",
 			},
 		},
 		{
-			name:           "alphanum with space",
-			referenceKey:   "reference",
-			referenceValue: "abc def",
-			expectedStatus: http.StatusBadRequest,
+			name:            "alphanum with space",
+			refKey:          "reference",
+			refValue:        "abc def",
+			refPointerKey:   "reference_pointer",
+			refPointerValue: "abc def",
+			expectedStatus:  http.StatusBadRequest,
 			expectedBodies: []string{
 				"Key: 'testStruct.Reference' Error:Field validation for 'Reference' failed on the 'alphanum_with_underscore_or_dash' tag",
+				"Key: 'testStruct.ReferencePointer' Error:Field validation for 'ReferencePointer' failed on the 'alphanum_with_underscore_or_dash' tag",
 			},
 		},
 		{
-			name:           "alphanum with underscore",
-			referenceKey:   "reference",
-			referenceValue: "abc123_def",
-			expectedStatus: http.StatusOK,
+			name:            "alphanum with underscore",
+			refKey:          "reference",
+			refValue:        "abc123_def",
+			refPointerKey:   "reference_pointer",
+			refPointerValue: "abc123_def",
+			expectedStatus:  http.StatusOK,
 			expectedBodies: []string{
 				`"reference":"abc123_def"`,
+				`"reference_pointer":"abc123_def"`,
 			},
 		},
 		{
-			name:           "alphanum with dash",
-			referenceKey:   "reference",
-			referenceValue: "abc123-def",
-			expectedStatus: http.StatusOK,
+			name:            "alphanum with dash",
+			refKey:          "reference",
+			refValue:        "abc123-def",
+			refPointerKey:   "reference_pointer",
+			refPointerValue: "abc123-def",
+			expectedStatus:  http.StatusOK,
 			expectedBodies: []string{
 				`"reference":"abc123-def"`,
+				`"reference_pointer":"abc123-def"`,
 			},
 		},
 		{
-			name:           "alphanum with underscore and dash",
-			referenceKey:   "reference",
-			referenceValue: "abc123_def-ghi",
-			expectedStatus: http.StatusOK,
+			name:            "alphanum with underscore and dash",
+			refKey:          "reference",
+			refValue:        "abc123_def-ghi",
+			refPointerKey:   "reference_pointer",
+			refPointerValue: "abc123_def-ghi",
+			expectedStatus:  http.StatusOK,
 			expectedBodies: []string{
 				`"reference":"abc123_def-ghi"`,
+				`"reference_pointer":"abc123_def-ghi"`,
 			},
 		},
 	} {
 		s.Run(testCase.name, func() {
-			req, err := http.NewRequest(http.MethodPost, testEndpoint, strings.NewReader(fmt.Sprintf(`{"%s":"%s"}`, testCase.referenceKey, testCase.referenceValue)))
+			req, err := http.NewRequest(http.MethodPost, testEndpoint,
+				strings.NewReader(fmt.Sprintf(`{"%s":"%s", "%s": "%s"}`,
+					testCase.refKey, testCase.refValue, testCase.refPointerKey, testCase.refPointerValue)))
 			s.NoError(err)
 
 			r := httptest.NewRecorder()
