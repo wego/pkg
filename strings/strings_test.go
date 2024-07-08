9cb6d153a9eeecd7d09d48c15c0c4700196a3a0f
Aaron Asuncion <aaron.asuncion@wego.com>
2024-07-08 09:57:02 +0800

    new-strings-package
    - added strings package to simplify blank/empty checking for string fields

---
+++ strings/strings_test.go
@@ -0,0 +1,77 @@
+package strings_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/wego/pkg/pointer"
+	"github.com/wego/pkg/strings"
+)
+
+func Test_IsBlankP(t *testing.T) {
+	assert := assert.New(t)
+	assert.True(strings.IsBlankP(nil))
+	assert.True(strings.IsBlankP(pointer.To("")))
+	assert.True(strings.IsBlankP(pointer.To("  ")))
+	assert.False(strings.IsBlankP(pointer.To("str")))
+	assert.False(strings.IsBlankP(pointer.To(" str ")))
+}
+
+func Test_IsBlank(t *testing.T) {
+	assert := assert.New(t)
+	assert.True(strings.IsBlank(""))
+	assert.True(strings.IsBlank("  "))
+	assert.False(strings.IsBlank("str"))
+	assert.False(strings.IsBlank(" str "))
+}
+
+func Test_IsNotBlankP(t *testing.T) {
+	assert := assert.New(t)
+	assert.False(strings.IsNotBlankP(nil))
+	assert.False(strings.IsNotBlankP(pointer.To("")))
+	assert.False(strings.IsNotBlankP(pointer.To("  ")))
+	assert.True(strings.IsNotBlankP(pointer.To("str")))
+	assert.True(strings.IsNotBlankP(pointer.To(" str ")))
+}
+
+func Test_IsNotBlank(t *testing.T) {
+	assert := assert.New(t)
+	assert.False(strings.IsNotBlank(""))
+	assert.False(strings.IsNotBlank("  "))
+	assert.True(strings.IsNotBlank("str"))
+	assert.True(strings.IsNotBlank(" str "))
+}
+
+func Test_IsEmptyP(t *testing.T) {
+	assert := assert.New(t)
+	assert.True(strings.IsEmptyP(nil))
+	assert.True(strings.IsEmptyP(pointer.To("")))
+	assert.False(strings.IsEmptyP(pointer.To("  ")))
+	assert.False(strings.IsEmptyP(pointer.To("str")))
+	assert.False(strings.IsEmptyP(pointer.To(" str ")))
+}
+
+func Test_IsEmpty(t *testing.T) {
+	assert := assert.New(t)
+	assert.True(strings.IsEmpty(""))
+	assert.False(strings.IsEmpty("  "))
+	assert.False(strings.IsEmpty("str"))
+	assert.False(strings.IsEmpty(" str "))
+}
+
+func Test_IsNotEmptyP(t *testing.T) {
+	assert := assert.New(t)
+	assert.False(strings.IsNotEmptyP(nil))
+	assert.False(strings.IsNotEmptyP(pointer.To("")))
+	assert.True(strings.IsNotEmptyP(pointer.To("  ")))
+	assert.True(strings.IsNotEmptyP(pointer.To("str")))
+	assert.True(strings.IsNotEmptyP(pointer.To(" str ")))
+}
+
+func Test_IsNotEmpty(t *testing.T) {
+	assert := assert.New(t)
+	assert.False(strings.IsNotEmpty(""))
+	assert.True(strings.IsNotEmpty("  "))
+	assert.True(strings.IsNotEmpty("str"))
+	assert.True(strings.IsNotEmpty(" str "))
+}
